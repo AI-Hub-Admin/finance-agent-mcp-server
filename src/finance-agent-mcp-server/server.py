8a03fdbe1a1865a418af491505a7bc59b202c573
AI-Agent-Hub <aihubagent@163.com>
2025-08-03 00:50:40 -0700

    Update server.py

--- src/finance-agent-mcp-server/server.py
+++ src/finance-agent-mcp-server/server.py
@@ -30,51 +30,57 @@ class CustomFastMCP(FastMCP):
 
 # Initialize FastMCP server
 server = CustomFastMCP(
-    "finance-agent-mcp-server",
-    prompt="""
-# Financial Agent MCP Server
-
-This server provides tools to get realtime financial data from global financial market, such as stock market in NASDAQ,NYSE,DOW, HKEX, Shanghai,Shenzhen, LSE, TSE, NSE India, etc.
-
-The API is free without need for keys, and the data sources are realtime crawling or public available APIs from websites such as nasdaq.com,morningstar.com,marketbeat.com, xueqiu.com, stockanalysis.com, etc. And the data from realtime fetched from website with proxy settings.
-
-## Available Tools
-
-### 1. get_stock_price_global_market
-
-Use this tool to get realtime or near-realtime finance data is crucial for success of AI Agents, this tool can help build a common interface of open API to get public available data from global Finance Market (US, Europe, Asia) with multiple finance investment choices such as Stock, Index, Option, etc. 
-Getting Realtime Data maybe blocked by website and this repo is not responsible for proxy or any data correctness related issues.
-
-
-## Guidelines for Use
-
-### Available Markets
-REGION  MARKET  INVESTMENT TYPE API DATA SOURCE
-United Status   US (NASDAQ,NYSE,DOW)    STOCK   morningstar.com
-United Status   US (NASDAQ,NYSE,DOW)    STOCK   zacks.com
-United Status   US (NASDAQ,NYSE,DOW)    STOCK   marketbeat.com
-Asia    HK (HKEX)   STOCK   hkex.com
-Asia    CN_MAINLAND (SHANGHAI SH, SHENZHEN SZ)  STOCK   xueqiu.com
-Asia    India (NSE) STOCK   moneycontrol.com
-Europe  London (LSE)    STOCK   stockanalysis.com
-Asia    Toyko (TSE) STOCK   NA
-
-## Output Format
-#### HK Stock Info
----------
-symbol|1024
-avg_price|49.650 HKD
-high|50.950 HKD
-low|47.600 HKD
-previous_close|50.850 HKD
-update_time|14 Oct 2024 18:33
-market_capitalization|214.06 B HKD
-pe_ratio|31.15
-source|HKEX, https://www.hkex.com.hk/Market-Data/Securities-Prices/Equities/Equities-Quote?sym=1024&sc_lang=en
-data_source|hkex.com
-
-""",
+    name="finance-agent-mcp-server"
 )
+
+@server.prompt("system_prompt")
+def system_prompt() -> str:
+    """
+    """
+    prompt="""
+    # Financial Agent MCP Server
+
+    This server provides tools to get realtime financial data from global financial market, such as stock market in NASDAQ,NYSE,DOW, HKEX, Shanghai,Shenzhen, LSE, TSE, NSE India, etc.
+
+    The API is free without need for keys, and the data sources are realtime crawling or public available APIs from websites such as nasdaq.com,morningstar.com,marketbeat.com, xueqiu.com, stockanalysis.com, etc. And the data from realtime fetched from website with proxy settings.
+
+    ## Available Tools
+
+    ### 1. get_stock_price_global_market
+
+    Use this tool to get realtime or near-realtime finance data is crucial for success of AI Agents, this tool can help build a common interface of open API to get public available data from global Finance Market (US, Europe, Asia) with multiple finance investment choices such as Stock, Index, Option, etc. 
+    Getting Realtime Data maybe blocked by website and this repo is not responsible for proxy or any data correctness related issues.
+
+
+    ## Guidelines for Use
+
+    ### Available Markets
+    REGION  MARKET  INVESTMENT TYPE API DATA SOURCE
+    United Status   US (NASDAQ,NYSE,DOW)    STOCK   morningstar.com
+    United Status   US (NASDAQ,NYSE,DOW)    STOCK   zacks.com
+    United Status   US (NASDAQ,NYSE,DOW)    STOCK   marketbeat.com
+    Asia    HK (HKEX)   STOCK   hkex.com
+    Asia    CN_MAINLAND (SHANGHAI SH, SHENZHEN SZ)  STOCK   xueqiu.com
+    Asia    India (NSE) STOCK   moneycontrol.com
+    Europe  London (LSE)    STOCK   stockanalysis.com
+    Asia    Toyko (TSE) STOCK   NA
+
+    ## Output Format
+    #### HK Stock Info
+    ---------
+    symbol|1024
+    avg_price|49.650 HKD
+    high|50.950 HKD
+    low|47.600 HKD
+    previous_close|50.850 HKD
+    update_time|14 Oct 2024 18:33
+    market_capitalization|214.06 B HKD
+    pe_ratio|31.15
+    source|HKEX, https://www.hkex.com.hk/Market-Data/Securities-Prices/Equities/Equities-Quote?sym=1024&sc_lang=en
+    data_source|hkex.com
+
+    """
+    return prompt
 
 @server.tool()
 def get_stock_price_global_market(
